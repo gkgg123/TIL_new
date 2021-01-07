de90a569bfadc1e1826f9f0c7d623e4cfb072c82
gkgg123 <gkggg123@gmail.com>
2021-01-07 21:28:41 +0900

    2178 solved

---
+++ 알고리즘/백준/2606_바이러스_version1.py
@@ -0,0 +1,30 @@
+def bfs(N):
+    stack = [1]
+    visted = [0]*(N+1)
+    cnt = 0
+    visted[1] = 1
+    while stack:
+        num = stack.pop()
+        cnt += 1
+        for ind in graph[num]:
+            if not visted[ind]:
+                visted[ind] = 1
+                stack.append(ind)
+    return cnt - 1
+
+
+
+
+
+
+
+N = int(input())
+M = int(input())
+graph = [[] for _ in range(N+1)]
+for _ in range(M):
+    a,b = map(int,input().split())
+    graph[a].append(b)
+    graph[b].append(a)
+
+
+print(bfs(N))
