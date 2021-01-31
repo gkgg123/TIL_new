d1958b90978d60cc22ea1f8004bc8b14c429be70
gkgg123 <gkggg123@gmail.com>
2021-01-31 20:53:32 +0900

    11403 modify

--- 알고리즘/백준/11403_경로_찾기_version1.py
+++ 알고리즘/백준/11403_경로_찾기_version1.py
@@ -2,12 +2,14 @@
 
 
 
+import sys
+
 N = int(input())
-arr = [list(map(int,input().split())) for _ in range(N)]
+arr = [list(map(int,sys.stdin.readline().split())) for _ in range(N)]
 for k in range(N):
     for x in range(N):
         for y in range(N):
-            if arr[x][k] + arr[k][y] == 2:
+            if arr[x][k] and arr[k][y]:
                 arr[x][y] = 1
 
 for i in range(N):
