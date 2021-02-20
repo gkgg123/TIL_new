6a8cf136de218c2ac200a6205eda0dbb4d254819
gkgg123 <gkggg123@gmail.com>
2021-02-20 23:06:19 +0900

    2470 solved

---
+++ 알고리즘/백준/2470_두_용액_version1.py
@@ -0,0 +1,26 @@
+N = int(input())
+
+
+arr = list(map(int,input().split()))
+
+arr.sort()
+
+start = 0
+end = N-1
+index_list = (arr[start],arr[end])
+result = arr[end]+arr[start]
+
+while start<end:
+    temp = arr[end]+arr[start]
+    if abs(result) > abs(temp):
+        result = temp
+        index_list = (arr[start],arr[end])
+        if result == 0:
+            break
+    if temp >= 0:
+        end -= 1
+    else:
+        start += 1
+
+
+print(*index_list)
