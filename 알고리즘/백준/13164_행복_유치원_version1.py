5b74d8b96bc0fa25291b9e59b344e80666e6b5f5
gkgg123 <gkggg123@gmail.com>
2021-03-20 19:31:34 +0900

    13164 solved

---
+++ 알고리즘/백준/13164_행복_유치원_version1.py
@@ -0,0 +1,9 @@
+N,K = map(int,input().split())
+arr = list(map(int,input().split()))
+arr.sort()
+diff = [0]*N
+for i in range(N-1):
+    diff[i] = arr[i+1] - arr[i]
+
+diff.sort()
+print(sum(diff[:len(diff)-(K-1)]))
