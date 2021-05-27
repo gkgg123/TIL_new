dabdb48174a99176a475b2760047fe2e83728025
gkgg123 <gkggg123@gmail.com>
2021-05-27 22:39:39 +0900

    modify 10159

--- 알고리즘/백준/10159_저울_version1.py
+++ 알고리즘/백준/10159_저울_version1.py
@@ -8,9 +8,7 @@ graph = [[0 for _ in range(N+1)] for i in range(N+1)]
 visited = [[True for _ in range(N+1)] for _ in range(N+1)]
 for _ in range(M):
     x,y = map(int,input().split())
-    graph[x][y] = -1
-    visited[x][y] = False
-    visited[y][x] = False
+    graph[x][y] = 1
 
 
 
@@ -19,21 +17,18 @@ for mid in range(1,N+1):
         for end in range(1,N+1):
             if start == end:
                 continue
-            if graph[start][end] > graph[start][mid] + graph[mid][end] and graph[start][mid] and graph[mid][end]:
-                graph[start][end] = graph[start][mid] + graph[mid][end]
+            if graph[start][mid] and graph[mid][end]:
+                graph[start][end] = 1
 
 result = []
-
-
 for start in range(1,N+1):
     cnt = 0
     for end in range(1,N+1):
         if start == end:
             continue
         else:
-            if graph[start][end] == 0 and graph[end][start] == 0:
+            if not (graph[start][end] or graph[end][start]):
                 cnt += 1
     result.append(cnt)
-
 for row in result:
     sys.stdout.write(str(row)+'\n')
