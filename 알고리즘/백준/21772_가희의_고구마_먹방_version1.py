a4c570dec8032f1b5a2eb94e06f7c4d5958f6259
gkgg123 <gkggg123@gmail.com>
2021-05-27 22:39:31 +0900

    modify 21772

--- 알고리즘/백준/21772_가희의_고구마_먹방_version1.py
+++ 알고리즘/백준/21772_가희의_고구마_먹방_version1.py
@@ -6,19 +6,25 @@ def dfs(node,time,eat):
     global T,result,R,C
     if time == T:
         result = max(result,eat)
+        if result == total_cnt or result == T:
+            print(result)
+            exit()
         return
     else:
-        result = max(result,eat)
+        vistied[node[0]][node[1]] = True
         for i in range(4):
             nx = node[0] + dx[i]
             ny = node[1] + dy[i]
-            if 0<=nx<R and 0<=ny<C and not vistied[nx][ny]:
-                vistied[nx][ny] = True
-                if arr[nx][ny] == 'S':
-                    dfs((nx,ny),time+1,eat+1)
-                else:
+            if 0<=nx<R and 0<=ny<C:
+                if vistied[nx][ny]:
                     dfs((nx,ny),time+1,eat)
-                vistied[nx][ny] = False
+                elif not vistied[nx][ny] and arr[nx][ny] != '#':
+                    vistied[nx][ny] = True
+                    if arr[nx][ny] == 'S':
+                        dfs((nx,ny),time+1,eat+1)
+                    else:
+                        dfs((nx,ny),time+1,eat)
+                    vistied[nx][ny] = False
 
 R,C,T = map(int,input().split())
 result = 0
@@ -27,18 +33,18 @@ vistied = [[False for _ in range(C)] for _ in range(R)]
 
 arr = []
 start = []
+total_cnt = 0
 for x in range(R):
     temp = list(input())
     for y in range(C):
-        if temp[y] == '#':
-            vistied[x][y] = True
-        elif temp[y] == 'G':
+        if temp[y] == 'G':
             start = (x,y)
+        elif temp[y] == 'S':
+            total_cnt += 1
     arr.append(temp)
 
 
 dx = [-1,1,0,0]
 dy = [0,0,-1,1]
-
 dfs(start,0,0)
 print(result)
