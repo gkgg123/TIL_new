465edaa9bbd1596b3f57b531296674d59a1fef6f
gkgg123 <gkggg123@gmail.com>
2021-06-16 22:39:13 +0900

    15561 solved

---
+++ 알고리즘/백준/15561_링크와_스타트_version1.py
@@ -0,0 +1,23 @@
+import sys
+from itertools import combinations
+def input():
+    return sys.stdin.readline().rstrip()
+
+
+N = int(input())
+
+
+arr = [list(map(int,input().split())) for _ in range(N)]
+row = [sum(i) for i in arr]
+col = [sum(i) for i in zip(*arr)]
+new_arr = [i+ j for i, j in zip(row, col)]
+total_sum = sum(new_arr)//2
+result = float('inf')
+for num in range(1,N//2+1):
+    for combi in combinations(new_arr,num):
+        result = min(result,abs(total_sum-sum(combi)))
+        if result == 0:
+            break
+    if result == 0:
+        break
+print(result)
