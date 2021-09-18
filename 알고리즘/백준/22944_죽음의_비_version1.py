e95d785328a015ae4cb9e9ce7897061e8e57d43c
gkgg123 <gkggg123@gmail.com>
2021-09-18 21:52:19 +0900

    modify solution 22944

--- 알고리즘/백준/22944_죽음의_비_version1.py
+++ 알고리즘/백준/22944_죽음의_비_version1.py
@@ -1,30 +1,28 @@
 import sys
-sys.setrecursionlimit(1000000)
 def input():
     return sys.stdin.readline().rstrip()
-
-def solution(node,hp,umbrella,dis):
+def solution(x,y,hp,umbrella,dis):
     global result
-    x,y = node
     ex,ey = end_node
     end_dis = abs(ex-x) + abs(ey-y)
     if end_dis <= hp + umbrella:
         result = min(result,dis+end_dis)
+        return
 
     for idx in range(umbrella_cnt):
         ux,uy = umbrella_list[idx]
-        if not visited_umbrella[idx]:
-            next_dis = abs(ux-x) + abs(uy-y)
-            if next_dis > umbrella + hp:
-                continue
-            visited_umbrella[idx] = True
-            next_hp = hp - max(0,next_dis-dis-1)
-            solution((ux,uy),next_hp,D-1,dis+next_dis)
-            visited_umbrella[idx] = False
+        next_dis = abs(ux-x) + abs(uy-y)
+        if visited_umbrella[idx] or next_dis > umbrella+hp:
+            continue
+        visited_umbrella[idx] = True
+        next_hp = hp
+        if umbrella < next_dis:
+            next_hp = hp - next_dis + umbrella
+        solution(ux,uy,next_hp,D,dis+next_dis)
+        visited_umbrella[idx] = False
 
 N,H,D = map(int,input().split())
 
-arr = []
 start_node = []
 end_node = []
 umbrella_list = []
@@ -38,12 +36,10 @@ for x in range(N):
             end_node = (x,y)
         elif temp[y] == 'U':
             umbrella_list.append((x,y))
-    arr.append(temp)
 result = float('inf')
 umbrella_cnt = len(umbrella_list)
 visited_umbrella = [False for _ in range(umbrella_cnt)]
-solution(start_node,H,0,0)
-
+solution(start_node[0],start_node[1],H,0,0)
 if result == float('inf'):
     print(-1)
 else:
