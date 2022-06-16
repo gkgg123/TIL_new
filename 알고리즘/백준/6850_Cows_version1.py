b24726f214ac5581ed06a9f01c4e693b853bbdf3
gkgg123 <gkggg123@gmail.com>
2022-06-16 22:34:09 +0900

    6850 solved

---
+++ 알고리즘/백준/6850_Cows_version1.py
@@ -0,0 +1,37 @@
+import sys
+def input():
+    return sys.stdin.readline().rstrip()
+class Point():
+    def __init__(self,x,y) -> None:
+        self.x = x
+        self.y = y 
+def get_ccw(p1,p2,p3):
+    return ((p2.x - p1.x)* (p3.y - p1.y) - ((p2.y-p1.y) * (p3.x-p1.x)))
+def get_slope(p1,p2):
+    if p1.x == p2.x:
+        return float('inf')
+    return (p1.y - p2.y)/(p1.x-p2.x)
+N = int(input())
+points = []
+for _ in range(N):
+    x,y = map(int,input().split())
+    points.append(Point(x,y))
+
+
+points.sort(key = lambda p : (-p.x,-p.y))
+
+hull = [points.pop()]
+points.sort(key= lambda p : (get_slope(p,hull[0]), p.x,p.y))
+
+for point in points:
+    while len(hull)>1 and get_ccw(hull[len(hull) - 2], hull[len(hull)-1],point)<=0:
+        hull.pop()
+    hull.append(point)
+
+stan = hull[-1]
+result = 0
+for i in range(len(hull)-2):
+    result += get_ccw(stan,hull[i],hull[i+1])
+
+result = abs(result)/2
+print(int(result//50))
