25308a34576bbff9f074261afb9f2c639ddf8887
gkgg123 <gkggg123@gmail.com>
2022-07-05 19:25:30 +0900

    1512 solved

--- 알고리즘/백준/1512_주기문으로_바꾸기_version1.py
+++ 알고리즘/백준/1512_주기문으로_바꾸기_version1.py
@@ -1,5 +1,5 @@
 import sys
-
+from collections import defaultdict
 def input():
     return sys.stdin.readline().rstrip()
 
@@ -7,7 +7,14 @@ def input():
 M = int(input())
 arr = list(input())
 N = len(arr)
+answer = float('inf')
 for le in range(M,0,-1):
-    for c in range(N-le-1):
-        print(arr[c] ,arr[c+le])
-    print('--?')
+    check = [defaultdict(int) for _ in range(le)]
+    for c in range(N):
+        check[c%le][arr[c]] += 1
+    cnt = 0
+    for i in range(le):
+        cnt = cnt + sum(check[i].values()) - max(check[i].values())
+    answer = min(answer,cnt)
+print(answer)
+        
